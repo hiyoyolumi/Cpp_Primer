bd0df893558a2366b05fa115be4203709f7f9f43
hiyoyolumi <862127279@qq.com>
2021-08-13 20:55:03 +0800

    first day to study c++

---
+++ c++_primer/exercise/chapter1/1_4.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+    int val1, val2;
+    std::cin >> val1 >> val2;
+    std::cout << "the sum of " << val1 << " * " << val2 
+              << " = " << val1 * val2 << std::endl;
+    std::cout << "hello" << std::endl;
+    return 0;
+}
---
+++ c++_primer/exercise/chapter1/1_5.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+    int val1, val2;
+    std::cin >> val1 >> val2;
+    std::cout << "the sum of" << std::endl;
+    std::cout << val1 << " * " << val2 << std::endl;
+    std::cout << " = " << val1 * val2 << std::endl;
+
+    return 0;   
+}
