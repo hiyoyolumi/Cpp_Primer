7d4d9cc08c16df449f7c8c225dc94a9ec57d4a76
crushd <862127279@qq.com>
2021-09-09 20:39:11 +0800

    2021-09-09

--- c++_primer/exercise/chapter3/3_24.cpp
+++ c++_primer/exercise/chapter3/3_24.cpp
@@ -6,5 +6,27 @@ using namespace std;
 int main()
 {
     vector<int> nums;
-    
+    int num;
+
+    while (cin >> num) {
+        nums.push_back(num);
+    }
+
+    for (auto a = nums.begin(); a < nums.end() - 1; a++) {
+        cout << *a + *(a+1) << " ";
+    }
+    cout << endl;
+
+    cout << "--------------" << endl;
+
+    auto a = nums.begin();
+    auto b = nums.end() - 1;
+    while (a < b) {
+        cout << *a + *b << " ";
+        a++;
+        b--;
+    }
+    cout << endl;
+
+    return 0;
 }
