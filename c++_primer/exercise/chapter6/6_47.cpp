1cb6d55bac76bf9dbdbeb0fc748d2633fb6b8a67
hiyoyolumi <862127279@qq.com>
2021-10-17 22:04:09 +0800

    2021-10-17

---
+++ c++_primer/exercise/chapter6/6_47.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+
+
+
+using namespace std;
+using Iter = vector<int>::const_iterator;
+
+void print(Iter first, Iter last)
+{
+#ifdef NDEBUG
+    cout << "vector size is: " << last - first << endl;
+#endif
+	if (first == last)
+	{
+		cout << "over!" << endl;
+		return;
+	}
+	cout << *first << " ";
+	print(++first, last);
+	
+}
+
+int main()
+{
+	vector<int> vec{ 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+	print(vec.cbegin(), vec.cend());
+
+	return 0;
+}
