de81ff524f586690aa71a65cb9d0cfcd8a59802c
crushd <862127279@qq.com>
2021-11-03 21:48:02 +0800

    2021-11-03

---
+++ DarkHorse/Polymorphism/1.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+
+//多态初探
+//虚函数 ---> 实现地址晚绑定
+
+using namespace std;
+
+class Animal
+{
+public:
+    virtual void speak()
+    {
+        cout << "Animal speak" << endl;
+    }
+};
+
+class Cat : public Animal
+{
+public:
+    void speak()
+    {
+        cout << "Cat speak" << endl;
+    }
+};
+
+void do_speak(Animal &animal)
+{
+    animal.speak();
+}
+
+int main()
+{
+    Cat cat;
+    do_speak(cat);
+    
+    Animal animal;
+    do_speak(animal);
+
+    return 0;
+}
