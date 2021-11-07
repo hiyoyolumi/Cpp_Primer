67b9e3415aa5abee62c097b5f345fdfa980ed854
hiyoyolumi <862127279@qq.com>
2021-11-07 21:59:15 +0800

    2021-11-07

--- DarkHorse/Polymorphism/2.cpp
+++ DarkHorse/Polymorphism/2.cpp
@@ -6,7 +6,7 @@ using namespace std;
 class Calculator
 {
 public:
-    virtual int Calcula()
+    virtual int getresult()
     {
         return 0;
     }
@@ -42,12 +42,52 @@ public:
     }
 };
 
+class divCalculator : public Calculator
+{
+public:
+    int getresult()
+    {
+        return m_numA / m_numB;
+    }
+};
+
+
+//多态使用条件
+//父类指针或者引用 指向 子类对象
 void test()
 {
-    
+    Calculator * abc = new addCalculator;
+    // addCalculator a;
+    // Calculator &abc = a;
+    abc->m_numA = 10;
+    abc->m_numB = 10;
+    cout << "m_numA + m_numB = " << abc->getresult() << endl;
+    delete abc;
+
+    abc = new subCalculator;
+    abc->m_numA = 10;
+    abc->m_numB = 10;
+    cout << "m_numA - m_numB = " << abc->getresult() << endl;
+    delete abc;
+
+    abc = new mulCalculator;
+    abc->m_numA = 10;
+    abc->m_numB = 10;
+    cout << "m_numA * m_numB = " << abc->getresult() << endl;
+    delete abc;
+
+    abc = new divCalculator;
+    abc->m_numA = 10;
+    abc->m_numB = 10;
+    cout << "m_numA - m_numB = " << abc->getresult() << endl;
+    delete abc;
+
+    return;
 }
 
 int main()
 {
+    test();
 
+    return 0;
 }
